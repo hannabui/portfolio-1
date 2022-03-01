1fed0736b6aa0a280b90bf638ebc9faef37366a0
hannabui <97701564+hannabui@users.noreply.github.com>
2022-02-28 19:16:22 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,11 @@
 <?php 
 if(isset($_POST["password"])) {
     if($_POST["password"] == "Studioghibli") {
-        echo("Welcome to the Studio Ghibli Gallery!");
+        echo("Welcome to the Studio Ghibli Gallery!  ");
+        echo 
+        '<a href="gallery.html">
+        Enter Gallery Here.
+        </a>';
     }
     else {
         echo("Please enter 'Studioghibli' and try again.");
